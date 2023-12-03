cf0c8ed1e8780148a05fc9c81d9ea0554d3ad1c3
Reland <reland.boyle@gmail.com>
2023-12-02 22:47:26 -0500

    Working nav rail; Developing themes

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:ft_test_app/temperature.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ft_test_app/temperature.dart';
+import 'package:ft_test_app/buildings.dart';
+import 'package:ft_test_app/units.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -13,39 +15,39 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  // Widget _selectedPage = const Placeholder();
+
   NavigationRailLabelType labelType = NavigationRailLabelType.all;
   bool showLeading = false;
   bool showTrailing = false;
   double groupAlignment = -1.0;
   bool mobile = false;
-  final String redLogo = './assets.red-blue-logo.svg';
+  final String redLogo = './assets.red-blue-icon.svg';
   late Widget redLogoSvg =
       SvgPicture.asset(redLogo, semanticsLabel: 'Acme Logo');
 
   @override
   Widget build(BuildContext context) {
     mobile = MediaQuery.of(context).size.width > 700 ? false : true;
-    int selectedIndex = 0;
-    Widget selectedPage;
-    switch (selectedIndex) {
+    Widget selectedPage = const Placeholder();
+
+    switch (_selectedIndex) {
       case 0:
         selectedPage = const TemperaturePage();
         break;
       case 1:
-        selectedPage = const Placeholder();
+        selectedPage = const BuildingsPage();
+        break;
+      case 2:
+        selectedPage = const UnitsPage();
         break;
       default:
-        throw UnimplementedError('no widget for $selectedIndex');
+        throw UnimplementedError('no widget for $_selectedIndex');
     }
 
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.primary,
-          leading: const IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: null,
-            icon: Icon(Icons.home),
-          ),
           title: Center(child: Text(widget.title)),
         ),
         body: Row(children: <Widget>[
@@ -62,23 +64,24 @@ class _MyHomePageState extends State<MyHomePage> {
             destinations: const <NavigationRailDestination>[
               NavigationRailDestination(
                 icon: Icon(Icons.thermostat_sharp),
+                selectedIcon: Icon(Icons.thermostat_sharp),
                 label: Text('Temperature'),
               ),
               NavigationRailDestination(
                 icon: Icon(Icons.business_sharp),
+                selectedIcon: Icon(Icons.business_sharp),
                 label: Text('Buildings'),
               ),
               NavigationRailDestination(
                 icon: Icon(Icons.chair_sharp),
+                selectedIcon: Icon(Icons.chair_sharp),
                 label: Text('Units'),
               ),
             ],
           ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(
-            child: Row(children: [
-              selectedPage
-            ]),
+            child: selectedPage,
           )
         ]));
   }
---
+++ lib/units.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UnitsPage extends StatefulWidget {
+  const UnitsPage({super.key});
+
+  @override
+  State<UnitsPage> createState() => _UnitsPageState();
+}
+
+class _UnitsPageState extends State<UnitsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
